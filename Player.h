f90c2800d043856d2e1a1e3b0c53c1bd63c47bce
DarkShinobi100 <37442282+DarkShinobi100@users.noreply.github.com>
2018-10-01 16:56:05 +0100

    Player.h file header created

---
+++ Player.h
@@ -0,0 +1,33 @@
+#pragma once
+// Library includes
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+//project include
+#include "AnimationSystem.h"
+
+class Player
+{
+
+public:
+	
+	Player();
+	void Input(sf::Event _gameEvent);
+	void Update(sf::Time _frameTime);
+	void Draw(sf::RenderTarget& _target);
+
+
+private:
+
+	//Assets
+	sf::Sprite m_sprite;
+	sf::Sound m_jumpSound;
+	sf::Sound m_deathSound;
+	sf::Sound m_landSound;
+	AnimationSystem m_animationSystem;
+	
+
+	// state
+	sf::Vector2f m_velocity;
+
+};
